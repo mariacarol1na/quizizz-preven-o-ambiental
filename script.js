31bfb5364d55eebd614d19c1d821720cc241b34c
mariacarol1na <128617169+mariacarol1na@users.noreply.github.com>
2024-08-06 12:00:46 -0300

    ok

--- script.js
+++ script.js
@@ -8,44 +8,68 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "1- qual a importancia da prevenção ambiental?
+        enunciado: "Qual a importancia da prevenção ambiental?
     ",
         alternativas: [
             {
-                texto: "a- causar impactos negativos na natureza",
+                texto: "Causar impactos negativos na natureza",
                 afirmacao: "Há não, ouve alteração do clima negativamente. "
             },
             {
-                texto: "b- preservar os recursos narurais para futuras gerações",
+                texto: " Preservar os recursos narurais para futuras gerações",
                 afirmacao: "Isso mesmo! Preservar a natureza melhora a qualidade de vida. "
             }
         ]
     },
     {
-        enunciado: "2-quai são algumas práticas de prevenção ambiental?",
+        enunciado: "Quai são algumas práticas de prevenção ambiental?",
         alternativas: [
             {
-                texto: "RESPOSTA a- reciclar materiais e reduzir o consumo de plásticos",
+                texto: "Reciclar materiais e reduzir o consumo de plásticos",
                 afirmacao: "Parabens! Recursos naturais bem geridos podem sustentar economias locais e globais a longo prazo."
             },
             {
-                texto: "b- descartar lixo em locais inadquados",
+                texto: "Descartar lixo em locais inadquados",
                 afirmacao: "Essa não, ouve perda e biodiversidade e desequilíbrio nos ecosistemas."
             }
         ]
     },
     {
-        enunciado:  "3-o que podemos fazer para contribuir com a prevenção ambiental?",
+        enunciado:  "O que podemos fazer para contribuir com a prevenção ambiental?",
         alternativas: [
             {
-                texto: "a- ignorar os problemas ambientais ",
+                texto: "Ignorar os problemas ambientais ",
                 afirmacao: "Ocorreu contaminção de ar, água e solo, afetando a saúde de seres vivos."
             },
             {
-                texto: "b- plantar árvores e participar de ações de limpeza ",
+                texto: "Plantar árvores e participar de ações de limpeza ",
                 afirmacao: "Isso mesmo, A conservação de florestas e outros ecossistemas ajudam a regular o clima."
             }
         ]
     },
       
 ];
+script.js 
+
+
+
+
+
+
+
+let atual=0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa;
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+mostraPergunta();
